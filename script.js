624d8ac2270e7f4948c72dbe937f334e61fd9bd4
parunchxi <parunchxi@gmail.com>
2023-06-04 16:29:08 +0700

    Format display error

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const data = JSON.parse(localStorage.getItem('Account'));
+
+// Show Error
+function showError(box, display, error) {
+    box.parentElement.classList.add('invalid');
+    display.innerHTML = error;
+    display.parentElement.style.display = 'block';
+}
+
+function hideError(box, display) {
+    box.parentElement.classList.remove('invalid');
+    display.innerHTML = '';
+    display.parentElement.style.display = 'none';
+}
+
 // Sign Up ---------------------------->
 const regUsername = document.getElementById('username');
 const regEmail = document.getElementById('email');
@@ -13,15 +28,11 @@ regUsername.addEventListener('input', () => {
     const usernameErrorDisplay = document.getElementById('username_error');
     const usernameRegEx = /^[A-Za-z0-9_.]{0,25}$/;
     if (regUsername.value.match(usernameRegEx)) {
-        regUsername.parentElement.classList.remove('invalid');
-        usernameErrorDisplay.innerHTML = '';
-        usernameErrorDisplay.parentElement.style.display = 'none';
+        hideError(regUsername, usernameErrorDisplay);
         rUchka = true;
     } else {
-        let usernameErrorText = 'Username can only use letters, number, underscores and periods.';
-        regUsername.parentElement.classList.add('invalid');
-        usernameErrorDisplay.innerHTML = usernameErrorText;
-        usernameErrorDisplay.parentElement.style.display = 'block';
+        showError(regUsername, usernameErrorDisplay,
+            'Username can only use letters, number, underscores and periods.');
         rUchka = false;
     }
 })
@@ -29,17 +40,13 @@ regUsername.addEventListener('input', () => {
 function checkUsername() {
     const usernameErrorDisplay = document.getElementById('username_error');
     if (regUsername.value !== '' && rUchka) {
-        regUsername.parentElement.classList.remove('invalid');
-        usernameErrorDisplay.innerHTML = '';
-        usernameErrorDisplay.parentElement.style.display = 'none';
+        hideError(regUsername, usernameErrorDisplay);
         rUchk = true;
     } else if (rUchka === false) {
         rUchk = false;
     } else {
-        let usernameErrorText = "Username can't be empty.";
-        regUsername.parentElement.classList.add('invalid');
-        usernameErrorDisplay.innerHTML = usernameErrorText;
-        usernameErrorDisplay.parentElement.style.display = 'block';
+        showError(regUsername, usernameErrorDisplay,
+            "Username can't be empty.");
         rUchk = false;
     }
 }
@@ -49,21 +56,15 @@ function checkEmail() {
     const emailErrorDisplay = document.getElementById('email_error');
     const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (regEmail.value.match(emailRegEx)) {
-        regEmail.parentElement.classList.remove('invalid');
-        emailErrorDisplay.innerHTML = '';
-        emailErrorDisplay.parentElement.style.display = 'none';
+        hideError(regEmail, emailErrorDisplay);
         rEchk = true;
     } else if (regEmail.value === '') {
-        let emailErrorText = "Email can't be empty.";
-        regEmail.parentElement.classList.add('invalid');
-        emailErrorDisplay.innerHTML = emailErrorText;
-        emailErrorDisplay.parentElement.style.display = 'block';
+        showError(regEmail, emailErrorDisplay,
+            "Email can't be empty.");
         rEchk = false;
     } else {
-        let emailErrorText = 'Invalid email';
-        regEmail.parentElement.classList.add('invalid');
-        emailErrorDisplay.innerHTML = emailErrorText;
-        emailErrorDisplay.parentElement.style.display = 'block';
+        showError(regEmail, emailErrorDisplay,
+            'Invalid email');
         rEchk = false;
     }
 }
@@ -71,15 +72,11 @@ function checkEmail() {
 function checkPassword() {
     const passwordErrorDisplay = document.getElementById('reg_password_error');
     if (regPassword.value !== '') {
-        regPassword.parentElement.classList.remove('invalid');
-        passwordErrorDisplay.innerHTML = '';
-        passwordErrorDisplay.parentElement.style.display = 'none';
+        hideError(regPassword, passwordErrorDisplay);
         rPchk = true;
     } else {
-        let emailErrorText = "Password can't be empty.";
-        regPassword.parentElement.classList.add('invalid');
-        passwordErrorDisplay.innerHTML = emailErrorText;
-        passwordErrorDisplay.parentElement.style.display = 'block';
+        showError(regPassword, passwordErrorDisplay,
+            "Password can't be empty.");
         rPchk = false;
     }
 }
@@ -88,15 +85,11 @@ function checkPassword() {
 regPasswordConfirm.addEventListener('change', () => {
     const passwordConfirmErrorDisplay = document.getElementById('conreg_password_error');
     if (regPasswordConfirm.value === regPassword.value) {
-        regPasswordConfirm.parentElement.classList.remove('invalid');
-        passwordConfirmErrorDisplay.innerHTML = '';
-        passwordConfirmErrorDisplay.parentElement.style.display = 'none';
+        hideError(regPasswordConfirm, passwordConfirmErrorDisplay);
         rPCchka = true;
     } else {
-        let passwordconfirmErrorText = 'Passwords do not match.';
-        regPasswordConfirm.parentElement.classList.add('invalid');
-        passwordConfirmErrorDisplay.innerHTML = passwordconfirmErrorText;
-        passwordConfirmErrorDisplay.parentElement.style.display = 'block';
+        showError(regPasswordConfirm, passwordConfirmErrorDisplay,
+            'Passwords do not match.');
         rPCchka = false;
     }
 })
@@ -104,17 +97,13 @@ regPasswordConfirm.addEventListener('change', () => {
 function checkPasswordConfirm() {
     const passwordConfirmErrorDisplay = document.getElementById('conreg_password_error');
     if (regPasswordConfirm.value !== '' && rPCchka) {
-        regPasswordConfirm.parentElement.classList.remove('invalid');
-        passwordConfirmErrorDisplay.innerHTML = '';
-        passwordConfirmErrorDisplay.parentElement.style.display = 'none';
+        hideError(regPasswordConfirm, passwordConfirmErrorDisplay);
         rPCchk = true;
-    } else if (rPCchka == false) {
+    } else if (regPasswordConfirm.value !== '' && rPCchka == false) {
         rPCchk = false;
     } else {
-        let passwordconfirmErrorText = "Passwords can't be empty.";
-        regPasswordConfirm.parentElement.classList.add('invalid');
-        passwordConfirmErrorDisplay.innerHTML = passwordconfirmErrorText;
-        passwordConfirmErrorDisplay.parentElement.style.display = 'block';
+        showError(regPasswordConfirm, passwordConfirmErrorDisplay,
+            "Passwords can't be empty.");
         rPCchk = false;
     }
 }
@@ -133,9 +122,9 @@ regCheck.addEventListener('click', () => {
 function saveData() {
     if (rUchk && rEchk && rPchk && rPCchk) {
         let newAccount = {
-            Username: regUsername.value,
-            Email: regEmail.value,
-            Password: regPassword.value
+            username: regUsername.value,
+            email: regEmail.value,
+            password: regPassword.value
         };
         account.push(newAccount);
         localStorage.setItem('Account', JSON.stringify(account));
